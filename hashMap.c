89f7355894a418307818b9fed90c4eec5dd07a57
andershelmsworth <andy.helmsworth@gmail.com>
2020-03-10 11:22:26 -0700

    test 2 working

--- hashMap.c
+++ hashMap.c
@@ -113,7 +113,7 @@ void hashMapCleanUp(HashMap* map)
                 currentLink = currentLink->next;
 
                 //Free the old link
-                hashLinkDelete(map, tempLink->key);
+                hashLinkDelete(tempLink);
 
                 //Set table to null if last element
                 if (currentLink == NULL) {
